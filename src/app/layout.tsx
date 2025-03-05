46a2def9df2c1cc840d19ffaf2a6905592eb11ba
Fatan Aminullah <fatan.aminullah.j@gmail.com>
2025-03-06 03:16:18 +0700

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-title" content="TeamSync" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
