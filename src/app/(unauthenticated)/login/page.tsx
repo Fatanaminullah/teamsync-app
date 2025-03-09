41b11bbfff135de9f37dc9c0c0458f2f399f0639
Fatan Aminullah <fatan.aminullah.j@gmail.com>
2025-03-09 18:41:57 +0700

    fix: dark mode login

--- src/app/(unauthenticated)/login/page.tsx
+++ src/app/(unauthenticated)/login/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { LoginForm } from "@/components/pages/login/login-form";
 import Image from "next/image";
+import logoMainDark from "@/public/img/img_logo-inverted.png";
+import logoMain from "@/public/img/img_logo-main.png";
+import { useTheme } from "next-themes";
 
 export default function LoginPage() {
+  const { theme } = useTheme();
   return (
     <div className="grid min-h-svh lg:grid-cols-2 -mt-20">
       <div className="flex flex-col gap-4 p-6 md:p-10">
@@ -11,12 +17,12 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block bg-slate-300">
+      <div className="relative hidden bg-muted lg:block dark:bg-background light:bg-slate-300">
         <Image
           fill
-          src="/img/img_logo-main.png"
+          src={theme === "dark" ? logoMainDark : logoMain}
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
     </div>
